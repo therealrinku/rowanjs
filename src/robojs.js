c4181b5ba6716a1774c5f70adfa53813151fbad9
therealrinku <rinkunited2032@gmail.com>
2025-03-08 20:59:20 +0545

    fix: json parse fixes and add playground html

--- src/robojs.js
+++ src/robojs.js
@@ -6,23 +6,25 @@
     const robojsState = new Map();
 
     // store state
-    const definedStates = document.querySelectorAll("[attribute='rjs-state']");
+    const definedStates = document.querySelectorAll("[rjs-state]");
     definedStates.forEach((ds) => {
-      const state = ds.getAttribute("rjs-state");
+      let state = ds.getAttribute("rjs-state");
+      state = state.replace(/'([^']+)'/g, '"$1"');
+      state = state.replace(/([a-zA-Z0-9_]+)(?=\s*:)/g, '"$1"');
       const obj = JSON.parse(state);
 
       for (const key in obj) {
-        robojsState.add(key, obj[key]);
+        robojsState.set(key, obj[key]);
       }
     });
 
     // automagically replace values from state
-    const statesToAdd = document.querySelectorAll(
-      "[attribute='rjs-state-value']",
-    );
+    const statesToAdd = document.querySelectorAll("[rjs-state-value]");
     statesToAdd.forEach((sta) => {
       const state = sta.getAttribute("rjs-state-value");
       sta.innerHTML = robojsState.get(state);
     });
   }
+
+  stateManager();
 })();
