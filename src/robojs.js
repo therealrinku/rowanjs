a26371170977ec89b2ae0baa0f997fed1f0f9f8e
therealrinku <rinkunited2032@gmail.com>
2025-03-10 22:48:02 +0545

    feat: move statemanager to class

--- src/robojs.js
+++ src/robojs.js
@@ -1,30 +1,32 @@
 (function robojs() {
-  console.log("initialized robojs");
+  class StateManager {
+    constructor() {
+      this.state = new Map();
+    }
 
-  // goal 1: state management 🔥
-  function stateManager() {
-    const robojsState = new Map();
+    initializeState() {
+      const definedStates = document.querySelectorAll("[rjs-state]");
+      definedStates.forEach((ds) => {
+        let state = ds.getAttribute("rjs-state");
+        state = state.replace(/'([^']+)'/g, '"$1"');
+        state = state.replace(/([a-zA-Z0-9_]+)(?=\s*:)/g, '"$1"');
+        const obj = JSON.parse(state);
 
-    // store state
-    const definedStates = document.querySelectorAll("[rjs-state]");
-    definedStates.forEach((ds) => {
-      let state = ds.getAttribute("rjs-state");
-      state = state.replace(/'([^']+)'/g, '"$1"');
-      state = state.replace(/([a-zA-Z0-9_]+)(?=\s*:)/g, '"$1"');
-      const obj = JSON.parse(state);
+        for (const key in obj) {
+          this.state.set(key, obj[key]);
+        }
+      });
+    }
 
-      for (const key in obj) {
-        robojsState.set(key, obj[key]);
-      }
-    });
-
-    // automagically replace values from state
-    const statesToAdd = document.querySelectorAll("[rjs-state-value]");
-    statesToAdd.forEach((sta) => {
-      const state = sta.getAttribute("rjs-state-value");
-      sta.innerHTML = robojsState.get(state);
-    });
+    updateState() {
+      const statesToAdd = document.querySelectorAll("[rjs-state-value]");
+      statesToAdd.forEach((sta) => {
+        const state = sta.getAttribute("rjs-state-value");
+        sta.innerHTML = this.state.get(state);
+      });
+    }
   }
-
-  stateManager();
+  const sm = new StateManager();
+  sm.initializeState();
+  sm.updateState();
 })();
