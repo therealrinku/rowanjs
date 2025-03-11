4ee465fa882445a5b362cd16a3a6cad59eaa240d
therealrinku <rinkunited2032@gmail.com>
2025-03-11 22:00:26 +0545

    feat: get updating state key dynamically (safely?)

--- src/robojs.js
+++ src/robojs.js
@@ -32,12 +32,26 @@
       for (const element of elements) {
         element.addEventListener("click", (_) => {
           const evalCode = element.getAttribute("rjs-onclick");
-          let a = this.state.get("a"); // HOW TO GET THIS DYNAMICALLY ???? i got no fking clue rn
-          eval(evalCode); // UNSAFE ??? most likely 
-          this.state.set("a", a);
+          const stateKey = this._extractStateKeyFromCode(evalCode);
+          const stateValue = this.state.get(stateKey);
+
+          window[stateKey] = stateValue; // leveraging window object here
+          eval(evalCode); // very very unsafe code
+
+          this.state.set(stateKey, window[stateKey]);
+          delete window[stateKey];
+
           this.updateState();
         });
       }
+    }
+
+    _extractStateKeyFromCode(code) {
+      //idk wth this regex is doing
+      const match = code.match(
+        /\b([a-zA-Z_][a-zA-Z0-9_]*)\s*(?=[\+\-\*\/\=\!%&\|\^]|$)/,
+      );
+      return match ? match[1] : null;
     }
   }
   const sm = new StateManager();
