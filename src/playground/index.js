9ea1a78204723e2778a4050e5f5eb878811a4385
therealrinku <rinkunited2032@gmail.com>
2025-04-17 13:55:46 +0545

    feat: atomic state update implementation

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,19 +1,22 @@
 (async function () {
   const { state, element } = window.rowanjs;
   const root = element("div");
-  root.addClass("bg-white h-[100vh] w-full");
+  root.addClass("bg-white h-[100vh] w-full flex items-center w-[100%]");
   root.createRoot();
 
   function createSidebar() {
     const sidebar = element("div");
     sidebar.addClass(
-      "bg-gray-700 w-[15vw] h-[100vh] p-2 flex flex-col gap-2 text-white",
+      "bg-gray-700 w-[15%] h-[100vh] px-3 py-2 flex flex-col gap-2 text-white text-sm",
     );
     root.append(sidebar);
     return sidebar;
   }
 
   const sidebar = createSidebar();
+  const main = element("main");
+  main.addClass("w-[85%] h-[100vh] bg-gray-100 px-5 py-2");
+  root.append(main);
 
   const a = state(1);
   const bojor = state();
@@ -27,18 +30,46 @@
 
   const button2 = element("button");
   button2.addClass("bg-green-500 px-5 py-1 text-white");
-  button2.setText("update bojor");
+  // button2.onClick(()=>bojor.set({...bojor.get(), name:"Poops"}))
+  button2.onClick(() =>
+    bojor.set({ ...bojor.get(), company: { name: "algolia" } }),
+  );
+  button2.setText("fetch users");
   sidebar.append(button2);
 
   const text = element("p");
-  text.setText(["Count is:", a]);
-  text.addDep(a);
+  text.setText(["Count is:", { state: bojor, key: "company.name" }]);
   sidebar.append(text);
 
-  const anothertext = element("p");
-  const ist = state(bojor.get().company.name);
-  button2.onClick(() => ist.set(ist.get() === "poof" ? "lms" : "poof"));
-  anothertext.setText(ist);
-  anothertext.addDep(ist);
-  sidebar.append(anothertext);
+  async function apicall() {
+    // const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+    // const users = await resp.json();
+    const users = [{ name: "Alex", id: 29, company: { name: "oops" } }];
+
+    const div = element("div");
+    div.addClass("flex flex-col gap-2");
+
+    const title = element("h5");
+    title.setText("Users list");
+    title.addClass("font-bold");
+    div.append(title);
+
+    for (const user of users) {
+      const elem = element("p");
+      const name = state(user.name);
+      const id = state(user.id);
+      elem.setText([name, id]);
+
+      const bt = element("button");
+      bt.setText("change");
+      bt.onClick(() => name.set(user.company.name));
+      elem.append(bt);
+
+      div.append(elem);
+    }
+
+    main.append(div);
+  }
+
+  // button2.onClick(apicall);
 })();
--- src/rowanjs.js
+++ src/rowanjs.js
@@ -4,66 +4,123 @@ class State {
     this.subscribers = new Array();
   }
 
-  __subscribe(elem) {
-    this.subscribers.push(elem);
-  }
-
-  __publish() {
-    for (const subscriber of this.subscribers) {
-      const v = subscriber.texts.map((txt) => {
-        return txt instanceof State ? txt.get() : txt;
-      });
-      console.log(subscriber.innerText, v);
-      subscriber.innerText = v.join(" ");
-    }
-  }
-
   get() {
     return this.state;
   }
 
+  __subscribe(elem) {
+    this.subscribers.push(elem);
+  }
+
+  __isPublishable(lastState, newState) {
+    if (this.subscribers.length === 0) {
+      return false;
+    }
+
+    let publishable = true;
+
+    this.subscribers.forEach((sub) => {
+      sub.textNodes.forEach((node) => {
+        if (typeof node === "object") {
+          const { state, key } = node;
+
+          if (!key) {
+            publishable = lastState !== newState && publishable;
+          }
+
+          let lv = lastState;
+          let nv = newState;
+
+          const props = key.split(".");
+
+          props.forEach((prop) => {
+            lv = lv[prop];
+            nv = nv[prop];
+          });
+
+          publishable = lv !== nv && publishable;
+        } else {
+          publishable = lastState !== newState && publishable;
+        }
+      });
+    });
+
+    return publishable;
+  }
+
+  __publish() {
+    this.subscribers.forEach((sub) => {
+      sub.__paint();
+    });
+  }
+
   set(value) {
+    const oldState = this.state;
     this.state = value;
-    this.__publish();
+
+    const publishable = this.__isPublishable(oldState, value);
+    if (publishable) {
+      this.__publish();
+    }
   }
 }
 
 class DOMElement {
   constructor(elem) {
     this.htmlelement = document.createElement(elem);
+    this.textNodes = new Array();
   }
 
-  setText(v) {
-    if (Array.isArray(v)) {
-      v.forEach((p) => this.setText(p));
+  __paint() {
+    console.log("repainting", this.htmlelement);
+    const texts = [];
+    this.textNodes.forEach((node) => {
+      if (typeof node === "object") {
+        const { state, key } = node;
+        if (!key) {
+          texts.push(state.get());
+        } else {
+          const props = key.split(".");
+          let val = state.get();
+
+          for (const prop of props) {
+            val = val[prop];
+          }
+
+          texts.push(val);
+        }
+      } else {
+        texts.push(node);
+      }
+    });
+
+    this.htmlelement.innerText = texts.join(" ");
+  }
+
+  setText(t) {
+    if (Array.isArray(t)) {
+      t.forEach((txt) => this.setText(txt));
       return this;
     }
 
-    if (!this.htmlelement.texts) {
-      this.htmlelement.texts = [];
+    if (typeof t === "object") {
+      const { state, key } = t;
+      if ((!state) instanceof State) {
+        throw new Error("Invalid state given", this.htmlelement);
+      }
+      state.__subscribe(this);
+      this.textNodes.push({ state, key });
+    } else {
+      this.textNodes.push(t);
     }
 
-    this.htmlelement.texts.push(v);
-
-    const renderedText = this.htmlelement.texts
-      .map((txt) => (txt instanceof State ? txt.get() : txt))
-      .join(" ");
-
-    this.htmlelement.innerText = renderedText;
+    this.__paint();
     return this;
   }
 
   addClass(classNames) {
     classNames = classNames.split(" ");
     this.htmlelement.classList.add(...classNames);
-    return this;
-  }
-
-  addDep(deps) {
-    deps = Array.isArray(deps) ? deps : [deps];
-    for (const dep of deps) {
-      dep.__subscribe(this.htmlelement);
-    }
     return this;
   }
 
